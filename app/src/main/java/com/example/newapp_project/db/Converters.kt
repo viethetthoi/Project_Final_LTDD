d69b28d773ee67cd7dcd6c40cf9018f0bbb1cf9a
viethetthoi <130229331+viethetthoi@users.noreply.github.com>
2024-06-07 05:56:32 +0700

    DataBase

--- app/src/main/java/com/example/newapp_project/db/Converters.kt
+++ app/src/main/java/com/example/newapp_project/db/Converters.kt
@@ -7,26 +7,12 @@ import com.google.gson.Gson
 class Converters {
     @TypeConverter
     fun fromSource(source: Source): String {
-        // Ví dụ: Chuyển đổi Source thành một chuỗi JSON
         return source.name
     }
 
     @TypeConverter
     fun toSource(name: String): Source {
-        // Ví dụ: Chuyển đổi chuỗi JSON thành đối tượng Source
         return Source(name, name)
     }
 }
-//class Converters {
-//    @TypeConverter
-//    fun fromSource(source: Source): String {
-//        // Ví dụ: Chuyển đổi Source thành một chuỗi JSON
-//        return Gson().toJson(source)
-//    }
-//
-//    @TypeConverter
-//    fun toSource(json: String): Source {
-//        // Ví dụ: Chuyển đổi chuỗi JSON thành đối tượng Source
-//        return Gson().fromJson(json, Source::class.java)
-//    }
-//}
+
